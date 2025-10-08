4e31bbfaebe7270c27307d323618fe7be73f1127
Lavanya <224241777+lavanya-s04@users.noreply.github.com>
2025-10-08 22:25:00 +0530

    Update Candidate_data.jsx
    phone number added

--- src/data/Candidate_data.jsx
+++ src/data/Candidate_data.jsx
@@ -1,15 +1,15 @@
 // Reusable sample candidate data
 const sampleCandidates = [
-  { name: "John Doe",email:"john23@gmail.com", role: "Frontend Developer", location: "Bangalore",job:"Full Time",
+  { name: "John Doe",email:"john23@gmail.com",phone: "9234567890", role: "Frontend Developer", location: "Bangalore",job:"Full Time",
     skills: ["HTML", "CSS", "JavaScript", "React"],resumeUrl: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" ,
     experience: "Internship at XYZ Solutions (6 months)", },
-  { name: "Jane Smith",email:"john23@gmail.com", role: "Backend Developer", location: "Hyderabad",job:"Full Time" ,
+  { name: "Jane Smith",email:"john23@gmail.com",phone: "9234567890", role: "Backend Developer", location: "Hyderabad",job:"Full Time" ,
     skills: ["HTML", "CSS", "JavaScript", "React"],
     experience: "Internship at XYZ Solutions (6 months)", },
-  { name: "Mike Johnson",email:"john23@gmail.com", role: "UI/UX Designer", location: "Delhi",job:"Full Time",
+  { name: "Mike Johnson",email:"john23@gmail.com",phone: "9234567890", role: "UI/UX Designer", location: "Delhi",job:"Full Time",
     skills: ["HTML", "CSS", "JavaScript", "React"],
     experience: "Internship at XYZ Solutions (6 months)", },
-  { name: "Sara Lee",email:"john23@gmail.com", role: "QA Engineer", location: "Mumbai",job:"Full Time", 
+  { name: "Sara Lee",email:"john23@gmail.com",phone: "9234567890", role: "QA Engineer", location: "Mumbai",job:"Full Time", 
     skills: ["HTML", "CSS", "JavaScript", "React"],
     experience: "Internship at XYZ Solutions (6 months)", }
    
